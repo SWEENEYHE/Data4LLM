022c03d742425fc6d2d541855e94ff1aacd65f6b
hx <549004247@qq.com>
2023-08-04 18:41:12 +0800

    [add] all

---
+++ Data4LLM.py
@@ -0,0 +1,466 @@
+import logging
+import os.path
+import random
+import re
+
+import jsonlines
+import pandas as pd
+from tqdm import tqdm
+import traceback
+import jieba
+from simhash import Simhash
+
+
+def setLogger(fileName):
+    # 配置日志记录器
+    logger = logging.getLogger(fileName)
+    logger.setLevel(logging.INFO)
+
+    fh = logging.FileHandler(fileName)
+    fh.setLevel(logging.DEBUG)
+
+    # ch = logging.StreamHandler()
+    # ch.setLevel(logging.INFO)
+
+    formater = logging.Formatter("%(message)s")
+    fh.setFormatter(formater)
+    # ch.setFormatter(formater)
+
+    logger.addHandler(fh)
+    # logger.addHandler(ch)
+    return logger
+
+
+'''
+    Data Process class for json & jsonline to generate data for LLM, including:
+    1.For row (sample)
+        1.1.process every row (rename property, remove extra property, add property...)
+        1.2.shuffle rows
+        1.3 remove duplicated rows
+        1.4 split train and test set
+    2.For file
+        2.1 transfer json to jsonline
+        2.2 merge jsonline files
+'''
+
+
+class Data4LLM:
+
+    # 封装保存过程
+    @classmethod
+    def __save_and_clear__(cls, writer, items, close=False):
+        writer.write_all(items)
+        items.clear()
+        if close:
+            writer.close()
+
+    '''
+    pre prorcess
+    '''
+
+    @classmethod
+    def __preprocess__(cls, file_input, process_fun, json=None):
+        if json is None:
+            json = file_input.split(".")[-1] == "json"
+            # 如果是json
+        if json:
+            reader = pd.read_json(file_input).iterrows()
+            # 预处理函数
+            pre_process = lambda x: x[1].to_dict()
+            # 由于iterrows()结果为（index,Series),包装一个适配器将Series取出并转换成dict，确保处理统一
+            process_fun = lambda x: process_fun(pre_process(x))
+            # 如果是jsonline
+        else:
+            reader = jsonlines.open(file_input)
+            pre_process = lambda x: x
+        return reader, pre_process, process_fun
+
+    @classmethod
+    def __get_simhash__(cls, text, stopwords_file="stopwords.txt", length=64):
+        # 1. 分词和去停用词
+        # 1. 加载停用词
+        stopwords = set()
+        with open(stopwords_file, 'r', encoding='utf-8') as f:
+            for line in f:
+                stopwords.add(line.strip())
+        words = [word for word in jieba.cut(text) if word not in stopwords]
+        words = " ".join(words)
+        simhash = Simhash(words, f=length)
+        return simhash
+
+    # 计算所有样本的simhash值，存储到文件中
+    @classmethod
+    def __simhash_all_rows__(cls, file_input, file_output=None, max_row_limit=1000, length=64):
+        '''
+        __simhash_all_rows__ : generate all the simhash value of the intput file and cache them to file
+        :param file_input: input file path
+        :param max_row_limit: the max data number in memory which is useful to save memory
+        :param length: the simhash length
+        :return:
+        '''
+        print("1.hash all the rows")
+        if file_output is None:
+            file_output = file_input.split(".")[0] + "_hash.jsonl"
+        reader = jsonlines.open(file_input)
+        writer = jsonlines.open(file_output, mode="w")
+        write_buffer = []
+
+        i = 0
+        for row in tqdm(reader, position=0):
+            row_text = " ".join(row.values())
+            hash_row = cls.__get_simhash__(row_text, length=length).value
+            row['hash'] = hash_row
+            write_buffer.append(row)
+
+            i += 1
+            if i % max_row_limit == 0:
+                cls.__save_and_clear__(writer, write_buffer)
+        cls.__save_and_clear__(writer, write_buffer, close=True)
+        return i
+
+    '''
+        1. For row(sample)
+    '''
+
+    @classmethod
+    def process_property(cls, file_input, file_output, process_fun, max_row_limit=1000, json=None):
+        reader, _, process_fun = cls.__preprocess__(file_input, process_fun, json)
+        writer = jsonlines.open(file_output, mode="a")
+
+        # 输出文件存在且大于0字节说明已经有内容，提示先删除 ： 由于进行追加，重复生成时导致非预期的结果，且很难排查（每次打开文件查看只看top k行以为新修改的逻辑没生效）
+        assert1 = os.path.exists(file_output)
+        assert2 = os.path.getsize(file_output) == 0
+        assert (not assert1) or (
+                assert1 and assert2), f"file_out_path {file_output} exists and is not empty, please remove it ahead!"
+
+        items = []
+        i = 0
+        for item in tqdm(reader, position=0):
+            try:
+                items.append(process_fun(item))
+                # 达到最大内存处理数，则写入文件
+                i += 1
+                if i % max_row_limit == 0:
+                    cls.__save_and_clear__(writer, items)
+            except Exception as e:
+                cls.__save_and_clear__(writer, items)
+                print(f"Error: encounter an error when dealing with the row {i + 1} : {e}")
+                traceback.print_exception()
+                break
+        # 最后一次写文件
+        cls.__save_and_clear__(writer, items, close=True)
+
+    @classmethod
+    def show_example(cls, file_input, process_fun, json=None, s=0, e=5):
+        '''
+        
+        :param file_input: 
+        :param process_fun: 
+        :param json: 
+        :param s: 
+        :param e: 
+        :return: 
+        '''
+        reader, preprocess_fun, process_fun = cls.__preprocess__(file_input, process_fun, json)
+        assert s <= e, f"s should >= e, but got s={s} <= e= {e}"
+        assert s >= 0, f"s should >= 0 , but got s={s}"
+
+        i = 0
+        for item in reader:
+            i += 1
+            if i >= e:
+                break
+            if i <= s:
+                continue
+            print(f"##### No {i} #####")
+            print(f"== Before ==\n{preprocess_fun(item)}")
+            print(f"== After ==\n{process_fun(item)}")
+
+    # shuffle
+    @classmethod
+    def shuffle(cls, file_input, file_output):
+        '''
+        shuffle: shuffle all the data in input file. warning: it loads all the data in memory
+        :param file_input: input data
+        :param file_output: output data
+        :return:
+        '''
+        reader = jsonlines.open(file_input)
+        writer = jsonlines.open(file_output, mode="w")
+        buffer = []
+        for row in reader:
+            buffer.append(row)
+
+        # shuffle
+        random.shuffle(buffer)
+        writer.write_all(buffer)
+
+
+    '''
+        1.3 remove duplicate data
+    '''
+
+    # 基于simhash比较去重（需要两两比较，时间复杂度高，不适合百万级别以上数据）
+    @classmethod
+    def remove_duplicate(cls, file_input, file_output, ratio=1, max_row_limit=1000, skip_hash=False, length=64,
+                         log_path="result.log"):
+        '''
+            remove_duplicate : remove duplicate data by sim_hash, which compares data one by one, getting more accurate and finely result but costing massive time
+            :param file_input: input file path with duplicated data
+            :param file_output: result file path
+            :param ratio: threshold for duplication, which is actually the distance of the two simhash value
+            :param max_row_limit: the max data number in memory which is useful to save memory
+            :param skip_hash: default false. it needed when call the function in first time, which is used to get the simhash in all the data
+            :param length: the simhash length
+            :param log_path: log file path
+            :return: result data number , removed data number
+        '''
+        logger = setLogger(log_path)
+        pbar = tqdm(position=0)
+        if not skip_hash:
+            # 1.hash all the rows
+            total = cls.__simhash_all_rows__(file_input, None, max_row_limit, length=length)
+            pbar = tqdm(total=total, position=0)
+
+        file_hash = file_input.split(".")[0] + "_hash.jsonl"
+
+        # 2.get all the distance
+        print("2.get all the distance")
+        reader1 = jsonlines.open(file_hash)
+        writer = jsonlines.open(file_output, mode="w")
+        rm_set = set()
+        write_buffer = []
+        len_total = 0
+        len_rm = 0
+        # 遍历第i个
+
+        for i, row1 in enumerate(reader1):
+            len_total += 1
+            # 如果第i个已经在移除集了，则无需计算
+            if i in rm_set:
+                continue
+
+            # 将第i个加入写列表
+            row1_hash = row1['hash']
+            row1.pop('hash')
+            write_buffer.append(row1)
+
+            if i % max_row_limit == 0:
+                cls.__save_and_clear__(writer, write_buffer)
+
+            # 遍历i+1，计算距离
+            reader2 = jsonlines.open(file_hash)
+            for j, row2 in enumerate(reader2):
+                if j <= i:
+                    continue
+                simhash1 = Simhash(row1_hash, f=length)
+                simhash2 = Simhash(row2['hash'], f=length)
+                distance = simhash1.distance(simhash2)
+                if distance < ratio:
+                    logger.log(level=logging.INFO, msg=f"or_row:{row1} \nrm_row:{row2}\ndis:{distance}\n====\n")
+                    rm_set.add(j)
+                    len_rm += 1
+
+            reader2.close()
+            pbar.update(1)
+            pbar.set_postfix(total=len_total, rm=len_rm)
+
+        cls.__save_and_clear__(writer, write_buffer, close=True)
+        # 删除临时的hash文件
+        if os.path.exists(file_hash):
+            os.remove(file_hash)
+        return len_total, len_rm
+
+    # 基于simhash做布尔筛去重（粗筛，速度快，适合百万级别以上数据）
+    @classmethod
+    def remove_duplicate_BloomFilter(cls, file_input, file_output, max_row_limit=1000, skip_hash=False, length=64,
+                                     log_path="result.log"):
+        '''
+            remove_duplicate : remove duplicate data by sim_hash, which removes data by bloom filter, very fast
+            :param file_input: input file path with duplicated data
+            :param file_output: result file path
+            :param max_row_limit: the max data number in memory which is useful to save memory
+            :param skip_hash: default false. it needed when call the function in first time, which is used to get the simhash in all the data
+            :param length: the simhash length
+            :param log_path: log file path
+            :return: result data number , removed data number
+        '''
+        logger = setLogger(log_path)
+        pbar = tqdm(position=0)
+        if not skip_hash:
+            # 1.hash all the rows
+            total = cls.__simhash_all_rows__(file_input, None, max_row_limit, length=length)
+            pbar = tqdm(total=total, position=0)
+
+        file_hash = file_input.split(".")[0] + "_hash.jsonl"
+
+        # 2.get all the distance
+        print("2.get all the distance")
+        reader1 = jsonlines.open(file_hash)
+        writer = jsonlines.open(file_output, mode="w")
+        keep_dict = {}
+        write_buffer = []
+        len_total = 0
+        len_rm = 0
+        # 遍历第i个
+        for i, row in enumerate(reader1):
+            len_total += 1
+            # 如果第i个hash已经在集合了，则无需计算
+            if row['hash'] in keep_dict:
+                len_rm += 1
+                logger.log(level=logging.INFO, msg=f"{i}\nor_row:{keep_dict[row['hash']]}\nrm_row:{row}")
+                continue
+
+            # 加入集合
+            keep_dict[row['hash']] = row
+            row.pop('hash')
+            # 将第i个加入写列表
+            write_buffer.append(row)
+
+            if i % max_row_limit == 0:
+                cls.__save_and_clear__(writer, write_buffer)
+
+            pbar.update(1)
+            pbar.set_postfix(total=len_total, rm=len_rm)
+
+        cls.__save_and_clear__(writer, write_buffer, close=True)
+        # 删除临时的hash文件
+        if os.path.exists(file_hash):
+            os.remove(file_hash)
+        return len_total, len_rm
+
+    '''
+
+    2.For file
+
+    '''
+
+    @classmethod
+    def merge_files(cls, files: list[str], file_output="merge.jsonl", shuffle=True, max_row_limit=1000):
+        '''
+        merge_files: merge all the files data, if
+        :param file_output:
+        :param files:
+        :param shuffle:
+        :param max_row_limit:
+        :return:
+        '''
+        writer = jsonlines.open(file_output, mode="a")
+
+        if not shuffle:
+            for file in files:
+                reader = jsonlines.open(file)
+                buffer = []
+                # 遍历行
+                for i, row in enumerate(reader):
+                    if i % max_row_limit == 0:
+                        writer.write_all(buffer)
+                        buffer.clear()
+                    buffer.append(row)
+                # 结束
+                writer.write_all(buffer)
+                buffer.clear()
+                writer.close()
+        else:
+            rows = []
+            # 同时打开多个文件
+            readers = [jsonlines.open(file) for file in files]
+            pbar = tqdm(position=0)
+            # readers池不为空，则持续循环
+            while len(readers) > 0:
+                for reader in readers:
+                    try:
+                        # 轮流提取行
+                        row = reader.read()
+                        rows.append(row)
+                        # 每max_row_limit条数据就写一次磁盘
+                        if len(rows) % max_row_limit == 0:
+                            cls.__save_and_clear__(writer, rows)
+                        pbar.update()
+                    except EOFError:
+                        # EOF表明到达终点，从池子里移除该reader
+                        readers.remove(reader)
+
+                # 每max_row_limit条数据就写一次磁盘
+                if len(rows) % max_row_limit == 0:
+                    cls.__save_and_clear__(writer, rows)
+            # 保存并关闭
+            cls.__save_and_clear__(writer, rows, close=True)
+            # shuffle
+            cls.shuffle(file_output, file_output)
+
+    @classmethod
+    def split_train_test(cls, file_input, train_test_ratio, file_train_output=None, file_test_output=None):
+        '''
+        split_train_test: split input file data to train set and test set by the train_test_ratio
+        :param file_input: input file path
+        :param train_test_ratio: train_test_ration=train number / total number = train_number / (train number+ test number)
+        :param file_train_output: default file_input.split(".")[0]+"_train.jsonl"
+        :param file_test_output: default file_input.split(".")[0]+"_test.jsonl"
+        :return: None
+        '''
+        file_base_name = file_input.split(".")[0]
+        if file_train_output is None:
+            file_train_output = file_base_name + "_train.jsonl"
+            file_test_output = file_base_name + "_test.jsonl"
+
+        reader = jsonlines.open(file_input)
+        test_writer = jsonlines.open(file_train_output, mode="w")
+        train_writer = jsonlines.open(file_test_output, mode="w")
+
+        buffer = []
+        for row in reader:
+            buffer.append(row)
+
+        row_num = len(buffer)
+        # shuffle
+        random.shuffle(buffer)
+        # count length
+        train_len = int(row_num * train_test_ratio)
+        # split
+        train_buffer = buffer[:train_len]
+        test_buffer = buffer[train_len:]
+        # shuffle
+        random.shuffle(train_buffer)
+        random.shuffle(test_buffer)
+        # save and close
+        train_writer.write_all()
+        train_writer.close()
+
+        test_writer.write_all(test_buffer)
+        test_writer.close()
+
+
+class F:
+    @classmethod
+    def rename(cls, row, map: dict[str:str]):
+        for k, v in map.items():
+            if v in row:
+                row[v] = row[k]
+                row.pop(k)
+
+    @classmethod
+    def replace(cls, row, pattern, repl, property=None):
+        if property is None:
+            property = row.keys()
+
+        for k in property:
+            row[k] = re.sub(pattern, repl, row[k])
+
+
+if __name__ == "__main__":
+    # Data4LLM.remove_duplicate(file_input="data/01_deal_llama_data.jsonl",
+    #                           file_output="result/rm_01_deal_llama_data.jsonl", ratio=1, max_row_limit=1000)
+    def process_fn(row: dict[str:str]):
+        # F.rename(row, {'input': 'prompt', 'output': 'chosen'})
+        F.replace(row, "#", "")
+        F.replace(row, "https?://\S+", "")
+        F.rename(row, {"input":"prompt","output":"chosen"})
+        # row['prompt'] = re.sub('apple', "banana", row['input'])
+        # row.pop('input')
+        return row
+
+
+    Data4LLM.show_example(file_input="data/test.jsonl", process_fun=process_fn)
+    # Data4LLM.process_property(file_input="data/01_deal_llama_data.jsonl",
+    # file_output="data/pp_01_deal_llama_data.jsonl", process_fun=process_fn)
--- README.md
+++ README.md
@@ -1,57 +1,56 @@
-# JsonProcess4LLM
-A JsonProcess libary for LLM data
+# Data4LLM
 
-# 1.usage
-## 1.1 process jsonl data
-### original data
+### The sample and useful data process tool for LLM finetuning
+now including: process for json & jsonline data and output jsonlines
+## Part
+
+## API
+### 1.For file
+#### (1) merge files
+merge all the jsonlines files with shuffle
 ```python
-from MyDataProcess import MyDataProcess
-file_in_path = "xxx.jsonl"
-file_out_path = "yyy.jsonl"
-#process function 处理逻辑
-def deal_fn(line:dict[str,str]):
-    line['input'] = line['instruction']
-    line.pop('instruction')
-    return line
-
-process = MyDataProcess(file_in_path,file_out_path,deal_fn,1000)
-process.show_example()
+import glob
+from Data4LLM import Data4LLM
+files = glob("dir/*.jsonl")
+Data4LLM.merge_files(files=files)
 ```
-
-````
-##### No 1 #####
-== Before ==
-{'instruction': '血热的临床表现是什么?', 'input': '', 'output': '初发或复发病不久。皮疹发展迅速，呈点滴状、钱币状或混合状。常见丘疹、斑丘疹、大小不等的斑片，潮红、鲜红或深红色。散布于体表各处或几处，以躯干、四肢多见，亦可先从头面开始，逐渐发展至全身。新皮疹不断出现，表面覆有银白色鳞屑，干燥易脱落，剥刮后有点状出血。可有同形反应;伴瘙痒、心烦口渴。大便秘结、小便短黄，舌质红赤，苔薄黄或根部黄厚，脉弦滑或滑数。血热炽盛病机，主要表现在如下四个面：一、热象：血热多属阳盛则热之实性、热性病机和病证、并表现出热象。二、血行加速：血得热则行，可使血流加速，且使脉道扩张，络脉充血，故可见面红目赤，舌色深红（即舌绛）等症。三、动血：在血行加速与脉道扩张的基础上，血分有热，可灼伤脉络，引起出血，称为“热迫血妄行”，或称动血。四、扰乱心神：血热炽盛则扰动心神，心主血脉而藏神，血脉与心相通，故血热则使心神不安，而见心烦，或躁扰发狂等症。'}
-== After ==
-{'input': '血热的临床表现是什么?', 'output': '初发或复发病不久。皮疹发展迅速，呈点滴状、钱币状或混合状。常见丘疹、斑丘疹、大小不等的斑片，潮红、鲜红或深红色。散布于体表各处或几处，以躯干、四肢多见，亦可先从头面开始，逐渐发展至全身。新皮疹不断出现，表面覆有银白色鳞屑，干燥易脱落，剥刮后有点状出血。可有同形反应;伴瘙痒、心烦口渴。大便秘结、小便短黄，舌质红赤，苔薄黄或根部黄厚，脉弦滑或滑数。血热炽盛病机，主要表现在如下四个面：一、热象：血热多属阳盛则热之实性、热性病机和病证、并表现出热象。二、血行加速：血得热则行，可使血流加速，且使脉道扩张，络脉充血，故可见面红目赤，舌色深红（即舌绛）等症。三、动血：在血行加速与脉道扩张的基础上，血分有热，可灼伤脉络，引起出血，称为“热迫血妄行”，或称动血。四、扰乱心神：血热炽盛则扰动心神，心主血脉而藏神，血脉与心相通，故血热则使心神不安，而见心烦，或躁扰发狂等症。'}
-````
-## 1.2 process json data
-### original data
-```
-   [ {
-        "instruction": "如果您是医生，请根据患者的描述回答医学问题。",
-        "input": "什么是药物的 \"生物钟\" 作用？\n\n",
-        "output": "生物钟是人体内一种特殊的生理节律系统，由许多细胞和分子组成，通过调节人体的代谢和生理过程来实现各种功能，包括睡眠-清醒节律、食欲、消化、内分泌和免疫反应等。\n\n药物的生物钟作用指的是一些药物可以通过调节人体内的生物钟来实现特殊的药效，例如在治疗失眠和抑郁症时，一些药物可以通过调节睡眠-清醒节律来改善患者的症状。还有一些药物可以通过调节内分泌和免疫反应来实现生物钟作用，例如调节甲状腺激素和抗体水平的药物等。生物钟作用对于人体的生理和药效具有重要意义，但也需要谨慎使用和监测不良反应。"
-    },
-    ....
-]
-```
+#### (2) split files to train and test file
 ```python
-from MyDataProcess import MyDataProcess
-
-file_in_path = "xxx.json"
-file_out_path = "yyy.jsonl"
-
-
-#处理逻辑
-def deal_fn(line: dict[str, str]):
-    line.pop('instruction')
-    return line
-
-deal = MyDataProcess(file_in_path, file_out_path, deal_fn, 1000, json=True)
-deal.show_example()
-deal.run()
+from Data4LLM import Data4LLM
+Data4LLM.split_train_test(file_input="data/test.json",train_test_ratio=3/5)
 ```
+### 2. For sample
+#### (1) shuffle
+```python
+from Data4LLM import Data4LLM
+Data4LLM.shuffle(file_input="data/test.jsonl")
+```
+#### (2) remove duplicated data
+```python
+from Data4LLM import Data4LLM
+Data4LLM.remove_duplicate_BloomFilter(file_input="data/test.json", file_output="result/rm_dup_test.json",length=64)
+```
+#### (3) process property in json like rename(input->prompt, output->chosen),replace unuseful characters
+```python
+from Data4LLM import Data4LLM,F
+def process_fn(row: dict[str:str]):
+    F.replace(row, "#", "")
+    F.replace(row, "https?://\S+", "")
+    F.rename(row, {"input":"prompt","output":"chosen"})
+    return row
+
+Data4LLM.process_property(file_input="data/test.jsonl", file_output="result/result_test.jsonl", process_fun=process_fn)
+```
+#### (4) show_example 
+```python
+from Data4LLM import Data4LLM,F
+def process_fn(row: dict[str:str]):
+    F.replace(row, "#", "")
+    F.replace(row, "https?://\S+", "")
+    F.rename(row, {"input":"prompt","output":"chosen"})
+    return row
+
+Data4LLM.show_example(file_input="data/test.jsonl", process_fun=process_fn)
 ```
 
-```
+
